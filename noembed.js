2732e4b2776834eebfe5a11adeffc0345955534d
Lee Aylward <lee@laylward.com>
2012-03-03 01:46:55 -0600

    initial commit

---
+++ noembed.js
@@ -0,0 +1,80 @@
+(function(){
+  var patterns = $([]);
+  var css = "https://noembed.com/noembed.css";
+  var oembed = "https://noembed.com/embed?url=";
+  var css_inserted = false;
+  var queue = [];
+ 
+  var update_patterns = function() {
+    $.ajax({
+      method : "get",
+      url : "https://noembed.com/providers",
+      dataType : "json",
+      success: function(data) {
+        if (!data) return;
+        $(data).each(function(i, provider) {
+          $(provider.patterns).each(function(j, pattern) {
+            patterns.push(new RegExp(pattern));
+          });
+        });
+        embed();
+      }
+    });
+  };
+
+  var insert_css = function() {
+    css_inserted = true;
+    $("<link/>", {
+      rel: "stylesheet",
+      type: "text/css",
+      href: css,
+    }).appendTo("head");
+  };
+
+  var find_links = function () {
+    $('#links a.url:not(.noembed-processed)').each(function(i, link) {
+      link = $(link);
+      link.addClass("noembed-processed");
+      patterns.each(function(j, pattern) {
+        if (pattern.test(link.attr('href'))) {
+          queue.push(link);
+        }
+      });
+    });
+  };
+
+  var embed = function () {
+    var link = queue.shift();
+
+    if (!link) {
+      setTimeout(embed, 3000);
+      return;
+    }
+
+    var href = link.attr('href');
+    if (!css_inserted) insert_css();
+
+    $.ajax({
+      method : "get",
+      url : oembed + encodeURI(href),
+      dataType: "json",
+      success: function(data) {
+        if (!data['html']) return;
+
+        var container = link.parents("div.links_main");
+        if (!container) return;
+
+        var html = $(data['html']);
+        html.css({marginTop: "0.5em"});
+        container.find(".snippet").remove();
+        container.append(html);
+      },
+      complete: function() {
+        setTimeout(embed, 500);
+      }
+    });
+  };
+
+  update_patterns();
+  setInterval(find_links, 1000);
+})();
